2b070f27b68c2257fe69c1e5f03fbd01db1059ec
Jakub-Biernat <k.biernat@vp.pl>
2024-03-25 18:36:36 +0100

    Moving player

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+FINISH_LINE_Y = 280
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.move, "w")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player.ycor() > FINISH_LINE_Y:
+        player.starting_position()
